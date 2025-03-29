0a19861851e51f867e1bda0f66d95b3f44a323c7
JemLBT <adesignjem@gmail.com>
2025-03-29 17:10:33 +0000

    trying again

--- src/Search.js
+++ src/Search.js
@@ -1,70 +1,48 @@
 import React, { useState } from "react";
-
-export default function Search() {
-  let apiKey = "842b36d55cb28eba74a018029d56b04c";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}units=metric`;
-  let [city, setCity] = useState("");
-  let [message, setMessage] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    setMessage(`The temperature in ${city} is 10°C`);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  return (
-    <div>
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-                autoFocus="on"
-                onChange={updateCity}
-              />{" "}
-            </div>
-            <div className="col-3">
-              <input type="submit" value="Search" />
-            </div>
-          </div>
-        </form>
-        <div className="row">
-          <div className="col-6">
-            <h1>New York</h1>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Wednesday 07:00</li>
-            <li>Mostly Cloudy </li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="weather icon"
-            />{" "}
-            <span className="temperature">6</span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity: 72%</li>
-              <li>Wind: 15 km/h</li>
-            </ul>
-          </div>
-        </div>{" "}
-      </div>
-
-      <h2>{message}</h2>
-    </div>
-  );
-}
+ 
+ export default function Search() {
+   let [city, setCity] = useState("");
+   let [message, setMessage] = useState("");
+ 
+   function handleSubmit(event) {
+     event.preventDefault();
+     setMessage(`The temperature in ${city} is 10°C`);
+   }
+ 
+   function updateCity(event) {
+     setCity(event.target.value);
+   }
+ 
+   return (
+     <div>
+       <form onSubmit={handleSubmit}>
+         <input type="search" placeholder="Enter a city" onChange={updateCity} />
+         <input type="submit" value="Search" />
+       </form>
+       <div className="Weather">
+         <h1>New York</h1>
+         <ul>
+           <li>Wednesday 07:00</li>
+           <li>Mostly Cloudy </li>
+         </ul>
+       </div>
+       <div className="row">
+         <div className="col-6">
+           <img
+             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+             alt="weather icon"
+           />{" "}
+           6°C{" "}
+         </div>{" "}
+       </div>
+       <div className="col-6">
+         <ul>
+           <li>Precipitation: 15%</li>
+           <li>Humidity: 72%</li>
+           <li>Wind: 15 km/h</li>
+         </ul>
+       </div>
+ 
+       <h2>{message}</h2>
+     </div>
+   );
