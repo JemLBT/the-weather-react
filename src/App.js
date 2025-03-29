e339dcdd2d59ca1917f0df745e05e59e167dfe93
JemLBT <adesignjem@gmail.com>
2025-03-29 14:24:37 +0000

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
-      <h3>
-        <a href="https://github.com/JemLBT/the-weather-react" target="blank">
-          Take me to git hub
-        </a>
-      </h3>
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <Search />
+        <h3>
+          <a href="https://github.com/JemLBT/the-weather-react" target="blank">
+            Take me to git hub
+          </a>
+        </h3>
+      </div>
     </div>
   );
 }
