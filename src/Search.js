15f8a1610f6332cf47d126d6adb86c71ac8299c7
JemLBT <adesignjem@gmail.com>
2025-03-29 14:40:32 +0000

    initial layout before css added

--- src/Search.js
+++ src/Search.js
@@ -19,6 +19,30 @@ export default function Search() {
         <input type="search" placeholder="Enter a city" onChange={updateCity} />
         <input type="submit" value="Search" />
       </form>
+      <div className="Weather">
+        <h1>New York</h1>
+        <ul>
+          <li>Wednesday 07:00</li>
+          <li>Mostly Cloudy </li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather icon"
+          />{" "}
+          6°C{" "}
+        </div>{" "}
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Precipitation: 15%</li>
+          <li>Humidity: 72%</li>
+          <li>Wind: 15 km/h</li>
+        </ul>
+      </div>
+
       <h2>{message}</h2>
     </div>
   );
