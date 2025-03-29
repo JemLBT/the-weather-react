ca598c81747f63d4cf86039a33def94fcb756769
JemLBT <adesignjem@gmail.com>
2025-03-29 15:43:31 +0000

    added API key

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Search from "./Search";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Weather Search Engine</h1>
-        <Search />
-        <h3>
-          <a href="https://github.com/JemLBT/the-weather-react" target="blank">
-            Take me to git hub
-          </a>
-        </h3>
+    <div>
+      <div className="App">
+        <div className="container">
+          <h1>Weather Search Engine</h1>
+          <Search />
+        </div>
       </div>
+      <p>
+        <a href="https://github.com/JemLBT/the-weather-react" target="blank">
+          This page was coded by Jem Thomas and is open sourced on Git-Hub
+        </a>
+      </p>
     </div>
   );
 }
